7765b3499febebfff51861eca278c14dcf82f003
theghostmac <theghostmac@gmail.com>
2023-11-12 20:33:39 +0100

    feature: update to main.c

--- main.c
+++ main.c
@@ -9,17 +9,6 @@
 int main() {
     stdio_init_all(); // Initialize stdio for printing to the console
 
-    // Initialize the Pico SDK runtime environment
-    gpio_init(LED_PIN);
-    gpio_set_dir(LED_PIN, GPIO_OUT);
-
-    while (1) {
-        // Toggle the LED
-        gpio_put(LED_PIN, 1);
-        sleep_ms(500);
-        gpio_put(LED_PIN, 0);
-        sleep_ms(500);
-    }
 
     return 0;
 }
